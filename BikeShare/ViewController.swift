563262df52b4dcd269fbee355557d480ad9e43be
Russell Weber <rwjk16@gmail.com>
2019-05-22 09:19:42 -0400

    Initial Commit

---
+++ BikeShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BikeShare
+//
+//  Created by Russell Weber on 2019-05-22.
+//  Copyright © 2019 Russell Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
